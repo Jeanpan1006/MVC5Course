d3da5c7bc3aaf7a8b034c1191af0b0551f3c4a08
jeanpan@livemail.tw <jeanpan@livemail.tw>
2016-03-05 13:45:48 +0800

    加了 註解

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -8,13 +8,20 @@ namespace MVC5Course.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
-            return View();
-        }
+            return View();　
+        } 
+        
+        //註解註解:first lesson
+        ////註解註解:first lesson
+        //註解註解:first lesson
+        //註解註解:first lesson
 
         public ActionResult About()
         {
+            //todo: UNDONE
             ViewBag.Message = "Your application description page.";
 
             return View();
